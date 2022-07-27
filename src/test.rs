635848227540a0072d685d4abe093fe78ea6cf2d
Simonas Kazlauskas <git@kazlauskas.me>
2022-07-27 13:35:51 +0300

    Move away from insta
    These snapshot crates have a strong preference towards panicking and
    outputting their diffs to stdout/err unconditionally, while also not
    really providing sufficient flexibility to integrate them into our
    vector-as-output shceme.
    
    It seemed easy enough to implement our own, so here we are.

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,7 @@
 use std::error;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
-use wast::parser::{self, Parse, ParseBuffer, Parser, Peek};
+use wast::parser::{self, Parse, ParseBuffer, Parser};
 use wast::token::Span;
 use wast::{kw, token};
 
@@ -106,20 +107,76 @@ impl WaftDirective<'_> {
             WaftDirective::AssertIndirectedWat { span, .. } => *span,
         }
     }
+}
 
-    fn display(&self) -> &'static str {
-        let text = match self {
-            WaftDirective::Module(_) => kw::module::display(),
-            WaftDirective::Skip { .. } => waft_kw::skip::display(),
-            WaftDirective::AssertGasWat { .. } => waft_kw::assert_instrumented_gas::display(),
-            WaftDirective::AssertStackWat { .. } => waft_kw::assert_instrumented_stack::display(),
-            WaftDirective::AssertInstrumentedWat { .. } => waft_kw::assert_instrumented::display(),
-            WaftDirective::AssertIndirectedWat { .. } => waft_kw::snapshot_indirected::display(),
-        };
-        text.strip_prefix("`")
-            .unwrap_or(text)
-            .strip_suffix("`")
-            .unwrap_or(text)
+#[derive(Debug)]
+pub(crate) struct DiffError {
+    diff: Vec<diff::Result<String>>,
+    path: Option<PathBuf>,
+}
+
+impl DiffError {
+    fn diff(old: &str, new: &str) -> Option<Self> {
+        if old == new {
+            return None;
+        }
+        let result = diff::lines(old, new);
+        if result.is_empty() {
+            None
+        } else {
+            let diff = result
+                .into_iter()
+                .filter_map(|s| {
+                    Some(match s {
+                        diff::Result::Left(l) => diff::Result::Left(l.into()),
+                        diff::Result::Right(r) => diff::Result::Right(r.into()),
+                        diff::Result::Both(l, r) if l != r => {
+                            diff::Result::Both(l.into(), r.into())
+                        }
+                        diff::Result::Both(_, _) => return None,
+                    })
+                })
+                .collect();
+            Some(Self { diff, path: None })
+        }
+    }
+    fn with_path(self, path: PathBuf) -> Self {
+        Self {
+            diff: self.diff,
+            path: Some(path),
+        }
+    }
+}
+impl std::error::Error for DiffError {}
+impl std::fmt::Display for DiffError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(path) = &self.path {
+            f.write_fmt(format_args!(
+                "non-empty differential against {}",
+                path.display()
+            ))?;
+        } else {
+            f.write_str("non-empty differential")?;
+        }
+        for line in &self.diff {
+            match line {
+                diff::Result::Left(l) => {
+                    f.write_str("\n      - ")?;
+                    f.write_str(&l)?;
+                }
+                diff::Result::Right(r) => {
+                    f.write_str("\n      + ")?;
+                    f.write_str(&r)?;
+                }
+                diff::Result::Both(l, r) => {
+                    f.write_str("\n      - ")?;
+                    f.write_str(&l)?;
+                    f.write_str("\n      + ")?;
+                    f.write_str(&r)?;
+                }
+            }
+        }
+        f.write_str("\n")
     }
 }
 
@@ -140,21 +197,37 @@ pub(crate) enum Error {
     InvalidOutput(#[source] wasmparser::BinaryReaderError, wast::Error),
     #[error("could not indirect the module {1}")]
     Indirect(#[source] indirection::Error, wast::Error),
-    #[error("snapshot comparison failed")]
-    Insta,
+
+    #[error("could not open the snapshot file {1:?}")]
+    OpenSnap(#[source] std::io::Error, PathBuf),
+    #[error("could not read the snapshot file {1:?}")]
+    ReadSnap(#[source] std::io::Error, PathBuf),
+    #[error("could not truncate the snapshot file {1:?}")]
+    TruncateSnap(#[source] std::io::Error, PathBuf),
+    #[error("could not seek the snapshot file {1:?}")]
+    SeekSnap(#[source] std::io::Error, PathBuf),
+    #[error("could not write the snapshot file {1:?}")]
+    WriteSnap(#[source] std::io::Error, PathBuf),
+    #[error("snapshot comparison failed {1}")]
+    DiffSnap(#[source] DiffError, wast::Error),
 }
 
 impl Error {
     pub(crate) fn set_path(&mut self, path: &Path) {
         match self {
-            Error::ReadTestContents(_, _) => {}
-            Error::FromUtf8(_, _) => {}
+            Error::ReadTestContents(_, _)
+            | Error::FromUtf8(_, _)
+            | Error::OpenSnap(_, _)
+            | Error::SeekSnap(_, _)
+            | Error::TruncateSnap(_, _)
+            | Error::ReadSnap(_, _)
+            | Error::WriteSnap(_, _) => {}
             Error::NewParseBuffer(s) => s.set_path(path),
             Error::ParseWaft(s) => s.set_path(path),
             Error::Indirect(_, s) => s.set_path(path),
             Error::InvalidModule(_, s) => s.set_path(path),
             Error::InvalidOutput(_, s) => s.set_path(path),
-            Error::Insta => {}
+            Error::DiffSnap(_, s) => s.set_path(path),
         }
     }
 }
@@ -279,7 +352,7 @@ impl<'a> TestContext<'a> {
         }
 
         // Now, execute all the assertions
-        for directive in &test.directives {
+        for (directive_num, directive) in test.directives.iter().enumerate() {
             let index = match directive {
                 WaftDirective::Module(_) => continue,
                 WaftDirective::Skip { .. } => return self.pass(),
@@ -342,46 +415,62 @@ impl<'a> TestContext<'a> {
                     }
                 }
             };
-
-            let mut insta = insta::Settings::new();
-            insta.set_prepend_module_to_snapshot(false);
-            insta.set_snapshot_path(
-                self.test_path
-                    .canonicalize()
-                    .expect("unsable to canonicalize the test path")
-                    .parent()
-                    .expect("could not get the parent directory of the test path")
-                    .join("snaps")
-            );
-            insta.set_snapshot_suffix(directive.display());
-            let (line, _) = directive.span().linecol_in(self.wast_data);
-            insta.bind(|| {
-                let _hook = std::panic::set_hook(Box::new(|_| {}));
-                let result = std::panic::catch_unwind(|| {
-                    insta::_macro_support::assert_snapshot(
-                        // Creates a ReferenceValue::Named variant
-                        insta::_macro_support::ReferenceValue::Named(Some(
-                            self.test_name.clone().into(),
-                        )),
-                        &output,
-                        env!("CARGO_MANIFEST_DIR"),
-                        "",
-                        &self.test_name,
-                        &self.test_path.display().to_string(),
-                        line as u32 + 1, // lines are 0-indexed here
-                        directive.display(),
-                    )
-                });
-                let _ = std::panic::take_hook();
-                match result {
-                    Ok(Ok(_)) => self.pass(),
-                    Ok(Err(error)) => return self.fail(&*error),
-                    Err(_panic) => return self.fail(Error::Insta),
-                }
-            });
+            match self.compare_snapshot(&directive, directive_num, output) {
+                Ok(()) => self.pass(),
+                Err(mut e) => self.fail_test_error(&mut e),
+            }
         }
 
         // Ensure we reported some sort of status.
         assert!(matches!(self.status, Status::Passed | Status::Failed));
+    }
+
+    fn compare_snapshot(
+        &mut self,
+        directive: &WaftDirective,
+        index: usize,
+        directive_output: String,
+    ) -> Result<(), Error> {
+        let should_update = std::env::var_os("SNAP_UPDATE").is_some();
+        let snaps_dir = self.test_path.parent().unwrap().join("snaps");
+        let snap_filename = format!("{}@{}.snap", self.test_name, index);
+        let snap_path = snaps_dir.join(snap_filename);
+        let mut snap_file = std::fs::OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .open(&snap_path)
+            .map_err(|e| Error::OpenSnap(e, snap_path.clone()))?;
+        let mut snap_contents = String::with_capacity(1024);
+        snap_file
+            .read_to_string(&mut snap_contents)
+            .map_err(|e| Error::ReadSnap(e, snap_path.clone()))?;
+        if let Some(error) = DiffError::diff(&snap_contents, &directive_output) {
+            if !should_update {
+                self.output.extend_from_slice(
+                    "note: run with SNAP_UPDATE environment variable to update".as_bytes(),
+                );
+                Err(Error::DiffSnap(
+                    error.with_path(snap_path),
+                    self.wast_error(directive.span(), String::new()),
+                ))
+            } else {
+                snap_file
+                    .set_len(0)
+                    .map_err(|e| Error::TruncateSnap(e, snap_path.clone()))?;
+                // TRICKY: If we don’t seek, the file will be filled with 0-bytes up to the
+                // current position (we read the file’s contents just now!) before the data is
+                // written...
+                snap_file
+                    .seek(SeekFrom::Start(0))
+                    .map_err(|e| Error::SeekSnap(e, snap_path.clone()))?;
+                snap_file
+                    .write_all(directive_output.as_bytes())
+                    .map_err(|e| Error::WriteSnap(e, snap_path))?;
+                Ok(())
+            }
+        } else {
+            Ok(())
+        }
     }
 }
