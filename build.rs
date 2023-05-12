f0e0c28e86634684b91a9c8963801f851f24cb1c
Léo Gaspard <leo@near.org>
2023-05-12 09:19:03 +0000

    have the build script not depend on opam

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn build_interpreter() {
     println!("cargo:rerun-if-changed=interpreter");
-    std::process::Command::new("opam")
-        .args(["exec", "--", "make", "-Cinterpreter", "wasm"])
+    std::process::Command::new("make")
+        .args(["-Cinterpreter", "wasm"])
         .output()
         .expect("failed compiling wasm interpreter");
 }
