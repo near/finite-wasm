c3d1e7f7a0307534ee5bbcaea08fe72f8218d1ad
Léo Gaspard <leo@near.org>
2023-05-16 18:31:58 +0000

    make fuzzer actually work

--- fuzz/compare_to_spec.rs
+++ fuzz/compare_to_spec.rs
@@ -1,16 +1,39 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use std::io::Write;
+use std::fmt::Write;
 use std::path::PathBuf;
 
-fuzz_target!(|data: wasm_smith::MaybeInvalidModule| {
+fuzz_target!(|data: wasm_smith::Module| {
+    let bytes = data.to_bytes();
+
+    let module = wasmparser::validate(&bytes).expect("wasm-smith-generated module is invalid");
+    if module.module_count() < 1 {
+        return;
+    }
+    let exports = &module
+        .module_at(0)
+        .expect("wasm-smith-generated module does not have one module")
+        .exports;
+
+    let mut wast_test = String::new();
+    write!(wast_test, "(module binary \"").unwrap();
+    for b in bytes.iter() {
+        write!(wast_test, "\\{:02}", b).unwrap();
+    }
+    write!(wast_test, "\")").unwrap();
+    for e in exports {
+        if matches!(e.1, wasmparser::types::EntityType::Func(_)) {
+            write!(wast_test, "(invoke {:?})", e.0).unwrap();
+        }
+    }
+
     let mut f = tempfile::Builder::new()
         .prefix("fuzzed-module-")
         .tempfile()
         .expect("creating temp file");
-    f.write_all(&data.to_bytes())
-        .expect("writing module to file");
+    std::io::Write::write_all(&mut f, wast_test.as_bytes()).expect("writing wast test file");
+
     let mut t = finite_wasm::wast_tests::test::TestContext::new(
         "fuzz".to_owned(),
         f.path().to_owned(),
@@ -18,5 +41,10 @@ fuzz_target!(|data: wasm_smith::MaybeInvalidModule| {
         PathBuf::from("/tmp"),
     );
     t.run();
-    assert!(!t.failed(), "test failed. Module was:\n{:?}", data);
+    assert!(
+        !t.failed(),
+        "Test failed.\nModule bytes were:\n{:?}\nWast test was:\n{}\n",
+        bytes,
+        wast_test,
+    );
 });
